92a7dd6dda0e5887049c2020513d19ef444d6ad7
Tsuki321 <103306466+Tsuki321@users.noreply.github.com>
2024-05-11 16:24:28 +0800

    Update CTFPlayerInventory_GetMaxItemCount.cpp
    Another fix

--- SEOwnedDE/SEOwnedDE/src/App/Hooks/CTFPlayerInventory_GetMaxItemCount.cpp
+++ SEOwnedDE/SEOwnedDE/src/App/Hooks/CTFPlayerInventory_GetMaxItemCount.cpp
@@ -1,6 +1,6 @@
 #include "../../SDK/SDK.h"
 
-MAKE_HOOK(CTFPlayerInventory_GetMaxItemCount, S::CTFPlayerInventory_GetMaxItemCount(), int, __fastcall, void* ecx, void* edx)
+MAKE_HOOK(CTFPlayerInventory_GetMaxItemCount, Signatures::CTFPlayerInventory_GetMaxItemCount.Get(), int, __fastcall, void* ecx, void* edx)
 {
 	return 3000;
 }
